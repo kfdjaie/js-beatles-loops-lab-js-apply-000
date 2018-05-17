cbdfffd5198e804781ea36747cde13f9571a4e13
Kristin F <kristinfehr@gmail.com>
2018-05-17 01:15:36 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,18 @@
+function theBeatlesPlay(musicians, instruments){
+  var empty = [];
+  for (i = 0; i < musicians.length; i++) {
+    empty[i] = musicians[i] + " plays " + instruments[0];
+  }
+}
+function johnLennonFacts(facts){
+  while (var i < facts.length){
+    i++;
+    return facts + "!!!";
+  }
+}
+function iLoveTheBeatles(num){
+  var empty2 = [];
+  do while () {
+    empty2[i] = "I love the Beatles!";
+  }
+}
